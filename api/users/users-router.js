0f60a4b96970a5d8419004e0c7cc3a8b16351ca6
Lia Moua <moua0061@yahoo.com>
2022-01-13 01:39:17 -0500

    first 2 endpoints working properly

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const Users = require('./users-model');
-const Posts = require('./../posts/posts-model');
+const User = require('./users-model');
+const Post = require('./../posts/posts-model');
 const { 
   logger, 
   validateUserId, 
@@ -14,7 +14,7 @@ const {
 const router = express.Router();
 
 router.get('/', logger, (req, res, next) => {
-  Users.get()
+  User.get()
     .then(users => {
       res.status(200).json(users);
     })
@@ -22,15 +22,15 @@ router.get('/', logger, (req, res, next) => {
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user)
+  res.json(req.user)
 });
 
-router.post('/', validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
-  console.log(req.name)
+router.post('/', validateUser, (req, res, next) => {
+  User.insert({ name: req.name})
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
